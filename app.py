b7c64c6a052330b44b8fcfd25d6e5495e747ca44
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2014-09-27 14:58:58 -0400

    Implemented websockets communication.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
+from flask.ext.socketio import SocketIO, emit
 import numpy
 import time
 
+from device import KeyboardDevice, BluetoothDevice
+
+from os import environ
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
 
 @app.route('/<device>(/<coordinate>)')
 def acceleration(device, coordinate = None):
@@ -12,5 +19,21 @@ def acceleration(device, coordinate = None):
 def ping():
     return 'Hello world sent at ' + str(time.time())
 
+@app.route('/devices')
+def devices():
+    return jsonify({d.identifier: str(d) for d in KeyboardDevice.scan()})
+
+@app.route('/<device>(/<coordinate>)')
+def listen(device):
+    pass
+
+@app.route('/<device>/connect')
+def connect(device):
+    g.devices.append(device)
+
+@socketio.on('connect')
+def test_connect():
+    emit('yo', {'data': 'Connected'})
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0')
